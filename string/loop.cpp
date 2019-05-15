94a453cae9d3c2f7fe1dfb8abaa9ccf9ff0c8b4c
Jack Xia <cijie@ualberta.ca>
2019-05-15 14:20:41 -0600

    loop though the string

---
+++ string/loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using std::cin;using std::cout;using std::endl;using std::string;
+
+int main(){
+    string str = "hello";
+    for (auto c :str){
+	    cout << c << endl;
+
+    }
+    return 0;
+
+
+
+
+}
