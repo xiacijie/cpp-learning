f9b0fd59e5fea4e8e401e4af39ae553d5ae1eac3
Jack Xia <cijie@ualberta.ca>
2019-05-15 13:28:29 -0600

    getline

---
+++ string/getline.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+
+    string line;
+    getline(cin,line);
+    cout << line << endl;
+    return 0;
+
+
+
+}
