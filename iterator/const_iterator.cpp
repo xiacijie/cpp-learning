1ee7f20f99eaf7b11486ee80664a29363c95c505
Jack Xia <cijie@ualberta.ca>
2019-05-17 11:10:40 -0600

    const iterator

---
+++ iterator/const_iterator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using std::string; using std::cout;
+using std::vector;
+int main(){
+    vector<int> v;
+    auto it3 = v.cbegin();
+    
+    return 0;
+}
