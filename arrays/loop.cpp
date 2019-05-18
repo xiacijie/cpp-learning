f088b0c3e8424dea1f64e2b45301dd57cdd24e69
xiacijie <cijie@ualberta.ca>
2019-05-17 21:40:33 -0600

    loop through the array

---
+++ arrays/loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using std::cout;
+
+int main(){
+    
+
+    int nums[] = {1,2,3,4,5};
+    for (auto i : nums){
+
+        cout << i ;
+    }
+
+    cout << '\n';
+
+    return 0;
+
+
+
+}
