0d74f8608a49ae501a5a2d4c34014d839549ff7b
xiacijie <cijie@ualberta.ca>
2019-05-20 13:50:00 -0600

    an object can be passed by value, ref and pointer

---
+++ class/object_pointer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+class A{
+    public:
+        int value = 1;
+
+};
+
+
+
+void printVal(A a){
+    a.value = 3;
+    cout << a.value << endl;
+}
+
+
+int main(){
+    A a;
+    a.value = 2;
+    printVal(a);
+    cout << a.value << endl;
+    return 0;
+}
