1fdb047c5cdc2d171dec0f4d64eeed89c8ff05ef
xiacijie <cijie@ualberta.ca>
2019-05-20 13:34:25 -0600

    polymorphism

---
+++ class/polymorphism.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+class Shape {
+    protected:
+        int width, height;
+
+    public:
+        Shape(int a = 0, int b = 0){
+            width = a;
+            height = b;
+        }
+
+        virtual int area(){
+            cout << "Parent Class Area: " << endl;
+            return 0;
+        }
+
+};
+
+
+
+class Rectangle: public Shape{
+    public:
+        Rectangle(int a = 0, int b = 0): Shape(a,b){}
+
+        int area(){
+            cout << "Rectangle class area: " << endl;
+            return width * height;
+        }
+
+};
+
+class Triangle: public Shape{
+    public:
+        Triangle( int a = 0, int b = 0 ):Shape(a,b){}
+
+        int area(){
+
+            cout << "Triangle class area: " << endl;
+            return (width * height) /2;
+        }
+};
+
+
+
+int main(){
+    Shape *shape;
+    Rectangle rec(10,7);
+    Triangle tri(10,5);
+
+    Shape *rec1 =new Rectangle(1,1);
+    rec1->area();
+
+    shape  =  & rec;
+    shape-> area();
+
+    shape = &tri;
+
+    shape-> area();
+    
+    return 0;
+
+}
+
+
